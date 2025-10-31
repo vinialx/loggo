24af4f6a2477e22b29966e8d86071debaaade3ef
viniciusaloise <vini.aloise.silva@gmail.com>
2025-10-31 01:28:50 -0300

    feat: create main interfaces

---
+++ src/interfaces/interfaces.ts
@@ -0,0 +1,31 @@
+export interface LoggoConfig {
+  client?: string;
+  directory?: string;
+  console?: boolean;
+  filecount?: number;
+  notify?: boolean;
+  smtp?: LoggoSMTPConfig;
+  throttle?: number;
+  debug?: boolean;
+}
+
+export interface LoggoSMTPConfig {
+  host: string;
+  port: number;
+  username: string;
+  password: string;
+  from: string;
+  to: string | string[];
+  secure: boolean;
+}
+
+export type LogLevel = "INFO" | "WARN" | "ERROR" | "CRITICAL" | "DEBUG";
+
+export interface LogEntry {
+  client: string;
+  level: LogLevel;
+  timestamp: string;
+  code: string;
+  module: string;
+  message: string;
+}
